d53ab5b3a94cc237294542dd9b9ac0133c65dcca
kate <kate.little@menloschool.org>
2024-08-30 08:38:39 -0700

    8/30 final message

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,11 @@
-Name: YOUR NAME HERE
+Name: KATE LITTLE
 
-| Date   |    Time    |                                                                    Update |
-|:-------|:----------:|--------------------------------------------------------------------------:|
-| Aug 22 |  8-8:30pm  | I started by thinking about my place in the universe.<br/>It was scary... |
-|        |            |                                                             And then I... |
+| Date   |    Time     |                                                                                                                                                            Update |
+|:-------|:-----------:|------------------------------------------------------------------------------------------------------------------------------------------------------------------:|
+| Aug 23 |  5-5:30pm   |                                         I tried to work out code #4, I tried to manually decode the message, assigning each emoji to a letter, but I had no luck. |
+| Aug 27 | 3:30-4:00pm |                                                                                     I implemented solutions to puzzles 1-3 using the logic we worked out in class |
+| Aug 27 | 4:00-4:30pm | I continued trying to decode #4, looking at different Unicode and decimal conversions, but I couldn't figure out how to convert unicode for an emoji into letters |
+| Aug 30 | 6:30-7:00am |                                                                      Using Mr. Blick's tips about unicode & a caesar shift, I implemented a solution to puzzle #4 |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/Riddler.java
+++ src/Riddler.java
@@ -1,42 +1,69 @@
+import static java.lang.Character.isLowerCase;
+import static java.lang.Character.isUpperCase;
+
 /**
  * The Riddler:
  * A puzzle by Zach Blick
  * for Adventures in Algorithms
  * At Menlo School in Atherton, CA
  *
- * Completed by: YOUR NAME HERE
+ * Completed by: Kate Little
  */
 public class Riddler {
 
     public String decryptOne(String encrypted) {
         String decrypted = "";
-
-        // TODO: Complete the decryptOne() function.
-
+        for (int i = 0; i < encrypted.length(); i++){
+            // Get each character of encrypted
+            char current = encrypted.charAt(i);
+            if (Character.isUpperCase(current)){
+                // Caesar shift on uppercase letters
+                decrypted += (char)((encrypted.charAt(i) - 'A' + 9) % 26 + 'A');
+                }
+            else if (Character.isLowerCase(current)){
+                // Caesar shift on lowercase letters
+                decrypted += (char)((encrypted.charAt(i) - 'a' + 9) % 26 + 'a');
+            }
+            // Maintains spaces & punctuation, aside from line breaks
+            else{
+                decrypted += current;
+            }
+        }
+        System.out.println(decrypted);
         return decrypted;
     }
 
+
     public String decryptTwo(String encrypted) {
         String decrypted = "";
-
-        // TODO: Complete the decryptTwo() function.
-
+        // Split the string based on spaces
+        String[] encryptedSplit = encrypted.split("\\s+");
+        for (int i = 0; i < encryptedSplit.length; i++){
+            // Take each segment's ascii value & add the corresponding letter to decrypted
+            int current = Integer.valueOf(encryptedSplit[i]);
+            decrypted += (char)current;
+        }
+        System.out.println(decrypted);
         return decrypted;
     }
 
     public String decryptThree(String encrypted) {
         String decrypted = "";
-
-        // TODO: Complete the decryptThree() function.
-
+        for (int i = 0; i < encrypted.length(); i+=8){
+            // Take 8-bit segment of encrypted, representing a char
+            String segment = encrypted.substring(i, i+8);
+            // Convert binary to decimal, then add to decrypted as a char
+            decrypted += (char) Integer.parseInt(segment,2);
+        }
         return decrypted;
     }
 
     public String decryptFour(String encrypted) {
         String decrypted = "";
-
-        // TODO: Complete the decryptFour() function.
-
+        for (int i = 0; i < encrypted.length(); i++){
+            // Caesar shift to convert each decimal value to ascii alphabet, then add to decrypted
+            decrypted += (char) ((int) encrypted.charAt(i) - 9919);
+        }
         return decrypted;
     }
 }
